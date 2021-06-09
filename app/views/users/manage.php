1884b2437a1823456cfb74289c8e083137b6c51f
Elion <emarku18@epoka.edu.al>
2021-06-09 20:11:42 +0200

    first commit elion

--- app/views/users/manage.php
+++ app/views/users/manage.php
@@ -1,6 +1,79 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
-<?php require APPROOT .'/views/inc/navbar.php'; ?>
-<a href="<?php echo URLROOT; ?>/users/manageUsers">Manage Users</a><br>
-    <a href="<?php echo URLROOT; ?>/rooms/index">Manage Rooms</a><br>
-    <a href="<?php echo URLROOT; ?>/products/index">Manage Products</a>
+<?php require APPROOT . '/views/inc/navbar.php'; ?>
+
+<article class="">
+    <div class="row row-cols-3 row-cols-sm-2 row-cols-md-3">
+        <div class="col">
+            <div class="card rounded border border-primary" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Manage Employees</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of
+                        the card's content.</p>
+                    <a href="<?php echo URLROOT; ?>/users/manageUsers" class="btn btn-primary">Manage Users</a><br>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+
+            <div class="card rounded border border-secondary" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title">Employee Info</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                        of the card's content.</p>
+                    <a href="<?php echo URLROOT; ?>/rooms/index" class="btn btn-primary">Manage Rooms</a><br>
+                </div>
+            </div>
+        </div>
+
+        <div class="col">
+            <div class="card rounded border border-danger" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"> Room Info</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                        of the card's content.</p>
+                    <a href="<?php echo URLROOT; ?>/products/index" class="btn btn-primary">Manage Products</a>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="card rounded border border-dark" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"> Hotel Finances </h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                        of the card's content.</p>
+                    <a href="<?php echo URLROOT; ?>/finances/index" class="btn btn-primary">Manage Finances</a>
+                </div>
+            </div>
+        </div>
+        <!-- <div class="col">
+            <div class="card rounded border border-primary" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"> Hotel Inventory </h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                        of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div> -->
+        <!-- <div class="col">
+            <div class="card border border-success" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                    <h5 class="card-title"> Public Relation </h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk
+                        of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div> -->
+    </div>
+
+</article>
+
+
 <?php require APPROOT . '/views/inc/footer.php'; ?>
--- app/views/users/manageUsers.php
+++ app/views/users/manageUsers.php
@@ -1,26 +1,35 @@
 <?php require APPROOT . '/views/inc/header.php'; ?>
 <?php require APPROOT . '/views/inc/navbar.php'; ?>
 <div>
-<table>
-        <tr>
-          <th>User ID</th>
-          <th>Username</th>
-          <th>Surname</th>
-          <th>Role</th>
-        </tr>
-<?php foreach($data['users'] as $user) : ?>
-  <tr>
-      <td><?php echo $user->employeeID;?></td>
-      <td><?php echo $user->Name;?></td>
-      <td><?php echo $user->Surname;?></td>
-      <td><?php echo $user->Role;?></td>
-      <td><a href="<?php echo URLROOT; ?>/users/editUser/<?php echo $user->employeeID; ?>">Edit</a></td> 
-      <td><a href="<?php echo URLROOT; ?>/users/deleteUser/<?php echo $user->employeeID; ?>">Delete</a></td> 
 
-  </tr>
-  <?php endforeach; ?>
+  <table class="table table-striped ">
+    <thead>
+      <tr>
+        <th>User ID</th>
+        <th>Username</th>
+        <th>Surname</th>
+        <th>Role</th>
+        <th>
+          <a href="<?php echo URLROOT; ?>/users/addUser">
+            <button type="button" class="btn btn-default" data-toggle="tooltip" data-placement="top" title="Add User" data-delay="0">
+              <?php echo file_get_contents("C:/xampp/htdocs/NewHotel/app/assets/user-plus-solid.svg"); ?>
+            </button>
+          </a>
+        </th>
+
+      </tr>
+    </thead>
+    <?php foreach ($data['users'] as $user) : ?>
+      <tr>
+        <td><?php echo $user->employeeID; ?></td>
+        <td><?php echo $user->Name; ?></td>
+        <td><?php echo $user->Surname; ?></td>
+        <td><?php echo $user->Role; ?></td>
+        <td><a href="<?php echo URLROOT; ?>/users/editUser/<?php echo $user->employeeID; ?>" class="btn btn-warning">Edit</a></td>
+        <td><a href="<?php echo URLROOT; ?>/users/deleteUser/<?php echo $user->employeeID; ?>" class="btn btn-danger">Delete</a></td>
+      </tr>
+    <?php endforeach; ?>
   </table>
-  </div>
+</div>
 
-<a href = "<?php echo URLROOT; ?>/users/addUser">Add User</a>
 <?php require APPROOT . '/views/inc/footer.php'; ?>
